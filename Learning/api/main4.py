ddda0c267829fddd3ddbc8d9f6a4457bd5eabfa2
satkr22 <satkr06@gmail.com>
2026-08-16 17:34:17 +0530

    learning

--- Learning/api/main4.py
+++ Learning/api/main4.py
@@ -1,17 +1,45 @@
+from typing import Any
+from typing_extensions import Self
+
 from fastapi import FastAPI, HTTPException, Path, Query
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 app = FastAPI()
 
 # request model
 class CreateUser(BaseModel):
-    user_name: str = Field(min_length=3, max_length=15)
+    username: str = Field(min_length=3, max_length=15, pattern=r"^[a-zA-Z0-9_]+$")
+    password: str = Field(min_length=7, max_length=30)
     age: int = Field(ge=18)
-
+    
+    @field_validator("username")
+    @classmethod
+    def validate_userame(cls, value: str):
+        if " " in value:
+            raise ValueError("Username cannot contain space")
+        return value
+    
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str):
+        if not any(char.isupper() for char in value):
+            raise ValueError("Password should has atleast one upper case character")
+        
+        if not any(char.isdigit() for char in value):
+            raise ValueError("Password should has atleast one number")
+        
+        if not any(char in value for char in ["@", "#", "$", "&", "_"]):
+            raise ValueError("Password should has atleast one special character")
+    
 # response model
 class UserResponse(BaseModel):
     name: str
     id: int
+    
+# response model
+class CreateUserResponse(BaseModel):
+    username: str
+    age: int
 
 @app.get("/")
 def root():
@@ -29,7 +57,14 @@ def get_users(user_id: int = Path(ge=1)):
         
     return {
         "id": user_id,
-        "name": "Satkr"
+    }
+    
+@app.post("/create_user", response_model=CreateUserResponse, status_code=201)
+def create_user(user: CreateUser):
+    return {
+        "username": user.username,
+        "age": user.age,
+        "password_raw": user.password
     }
 
 @app.get("/products")
