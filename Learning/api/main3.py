37c3da82e01a98f2b2dfada34c12c2ae130a7b2c
satkr22 <satkr06@gmail.com>
2026-08-16 13:14:06 +0530

    learning

--- Learning/api/main3.py
+++ Learning/api/main3.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# request body model
 class Product(BaseModel):
     title: str
     price: float
     stock: int
-    
+
+# response model
+class UserResponse(BaseModel):
+    name: str
+    age: int
+
 @app.get("/")
 def root():
     return {"message": "this is root"}
@@ -15,3 +21,12 @@ def root():
 @app.post("/products")
 def create_product(product: Product):
     return product
+
+
+@app.get("/users", response_model=UserResponse)
+def get_users():
+    return {
+        "name": "boss",
+        "age": 23,
+        "password": "12345@"
+    }
