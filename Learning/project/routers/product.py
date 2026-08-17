65a52dd6a2d62ab1348f417085d04ce09f98b773
satkr22 <satkr06@gmail.com>
2026-08-17 13:36:58 +0530

    project example 1

--- Learning/project/routers/product.py
+++ Learning/project/routers/product.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/products")
+router = APIRouter(
+    prefix="/products",
+    tags=["Products"]
+)
 
 @router.get("")
 def get_products():
--- Learning/project/routers/user.py
+++ Learning/project/routers/user.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/users")
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
 @router.get("")
 def get_users():
