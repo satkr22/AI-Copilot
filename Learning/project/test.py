41b32238b7a2026412aa61863811883e451bad2b
satkr22 <satkr06@gmail.com>
2026-08-19 10:55:04 +0530

    Project Example 1

---
+++ Learning/project/test.py
@@ -0,0 +1,11 @@
+from utils.password import hash_password, verify_password
+
+password = "secret123"
+
+hashed = hash_password(password=password)
+
+print("Original: ", password)
+print("Hash: ", hashed)
+
+print(verify_password(password=password, hashed_password=hashed))
+print(verify_password(password="wrongsecret123", hashed_password=hashed))
