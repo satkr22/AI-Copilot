df8e296f23bcab881f16d40434d1dea753d83884
satkr22 <satkr06@gmail.com>
2026-08-28 12:30:52 +0530

    db engine added

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,5 +1,15 @@
 from app.core.config import settings
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 database_url = settings.DATABASE_URL
-engine = create_engine
+
+engine = create_engine(
+    url=database_url,
+    echo=True
+)
+
+session = sessionmaker(
+    bind=engine,
+    autoflush=False
+)
